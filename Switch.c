846877c394f52d3b6eea9044718cd3201fda1b34
Karan <karan4c6@gmail.com>
2013-08-28 23:05:57 +0530

    switch case in C

---
+++ Switch.c
@@ -0,0 +1,76 @@
+  /**  
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    * 
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    * 
+    * Copyright 2013 Maven Scientists http://mavenscientists.com
+    */
+
+ /**
+   * @author	Maven Scientists (http://mavenscientists.com) 
+   * @email	info@mavenscientists.com
+   * Switch Case - This program will ask user to enter a number and 
+   * will display the corresponding month. If the month number in invalid,
+   * it will display "Invalid Input" 
+   */
+
+#include <stdio.h>
+
+int main()
+{
+	int month;
+	
+	printf("Enter Month Number : ");
+	scanf("%d", &month);
+	
+	switch(month)
+	{
+		case 1:
+			printf("January");
+			break;
+		case 2:
+			printf("February");
+			break;
+		case 3:
+			printf("March");
+			break;
+		case 4:
+			printf("April");
+			break;
+		case 5:
+			printf("May");
+			break;
+		case 6:
+			printf("June");
+			break;
+		case 7:
+			printf("July");
+			break;
+		case 8:
+			printf("August");
+			break;
+		case 9:
+			printf("September");
+			break;
+		case 10:
+			printf("October");
+			break;
+		case 11:
+			printf("November");
+			break;
+		case 12:
+			printf("December");
+			break;
+		default:
+			printf("Invalid Input");
+	} // end of switch...
+	
+	return 0;
+}
+
